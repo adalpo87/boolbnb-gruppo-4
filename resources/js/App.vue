87bfd8c62f01a1d261b272d6c9cf7734b8969090
adalpo87 <adalpo87@gmail.com>
2021-07-15 17:28:04 +0200

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+   
+   <h1>BOOLBNB</h1>
+   
+
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+
+window.Vue = require('vue');
+
+import App from './App.vue';
+
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
+
+
